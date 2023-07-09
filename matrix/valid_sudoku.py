452f9db84d02f8d0cb09313a3656b34679750ca6
Billy Russell <billy.hrussell@gmail.com>
2023-07-08 18:30:08 -0700

    leetcode practice

--- matrix/valid_sudoku.py
+++ matrix/valid_sudoku.py
@@ -47,35 +47,82 @@ matrix = [["5","3",".",".","7",".",".",".","."]
 
 
 
-def isValidSudoku(board):
+# def isValidSudoku(board):
+#     """
+#     :type board: List[List[str]]
+#     :rtype: bool
+#     """
+
+#     # create empty set
+#     rows = [set() for x in range(9)]
+#     columns = [set() for x in range(9)]
+#     squares = [[set() for x in range(3)] for y in range(3)]
+
+#     print(rows)
+
+#     for x in range(9):
+#         for y in range(9):
+#             cell_value = board[x][y]
+
+#             if cell_value == ".":
+#                 continue
+#             if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
+#                 return False
+
+#             rows[x].add(cell_value)
+#             columns[y].add(cell_value)
+#             squares[x//3][y//3].add(cell_value)
+
+#     print("rows ", rows)
+#     return True
+
+# print(isValidSudoku(matrix))
+
+
+def isValidSudoku(self, board):
     """
     :type board: List[List[str]]
     :rtype: bool
     """
 
-    # create empty set
-    rows = [set() for x in range(9)]
-    columns = [set() for x in range(9)]
-    squares = [[set() for x in range(3)] for y in range(3)]
+    # get column
+    # get box
 
-    print(rows)
-
-    for x in range(9):
-        for y in range(9):
-            cell_value = board[x][y]
-
-            if cell_value == ".":
+    cols = [[] for _ in range(9)]
+    boxes = []
+    # get cols
+    for row in board:
+        for i, val in enumerate(row):
+            if val == '.':
                 continue
-            if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
-                return False
+            else:
+                cols[i].append(val)
 
-            rows[x].add(cell_value)
-            columns[y].add(cell_value)
-            squares[x//3][y//3].add(cell_value)
+    # get boxes
+    # this iterates over a tuple,
+    # i is 0, then 3, then 6
+    for i in (0, 3, 6):
+        for j in (0, 3, 6):
+            box = [board[x][y] for x in range(i, i+3) for y in range(j, j+3)]
+            boxes.append(box)
 
-    print("rows ", rows)
+    def is_valid(unit):
+        unit = [i for i in unit if i != '.']
+
+        # if there are duplicates, set() will remove them.
+        return len(set(unit)) == len(unit)
+
+
+    for row in board:
+        if is_valid(row) == False:
+            return False
+
+    for col in cols:
+        if is_valid(col) == False:
+            return False
+
+    for box in boxes:
+        if is_valid(box) == False:
+            return False
+
     return True
-
-print(isValidSudoku(matrix))
-
-
