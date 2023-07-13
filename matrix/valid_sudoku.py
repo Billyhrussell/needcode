659773e7b0552d98e4d061a40a4bf29aa5dcbf0f
Billy Russell <billy.hrussell@gmail.com>
2023-07-12 17:59:34 -0700

    practice

--- matrix/valid_sudoku.py
+++ matrix/valid_sudoku.py
@@ -30,11 +30,135 @@
 
 #     for i in range(3):
 #         for j in range(3):
-#             box[i]
+# #             box[i]
 
-#     print(box)
+# #     print(box)
 
-matrix = [["5","3",".",".","7",".",".",".","."]
+# matrix = [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+
+
+
+
+# # def isValidSudoku(board):
+# #     """
+# #     :type board: List[List[str]]
+# #     :rtype: bool
+# #     """
+
+# #     # create empty set
+# #     rows = [set() for x in range(9)]
+# #     columns = [set() for x in range(9)]
+# #     squares = [[set() for x in range(3)] for y in range(3)]
+
+# #     print(rows)
+
+# #     for x in range(9):
+# #         for y in range(9):
+# #             cell_value = board[x][y]
+
+# #             if cell_value == ".":
+# #                 continue
+# #             if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
+# #                 return False
+
+# #             rows[x].add(cell_value)
+# #             columns[y].add(cell_value)
+# #             squares[x//3][y//3].add(cell_value)
+
+# #     print("rows ", rows)
+# #     return True
+
+# # print(isValidSudoku(matrix))
+
+
+# def isValidSudoku(self, board):
+#     """
+#     :type board: List[List[str]]
+#     :rtype: bool
+#     """
+
+#     # get column
+#     # get box
+
+#     cols = [[] for _ in range(9)]
+#     boxes = []
+#     # get cols
+#     for row in board:
+#         for i, val in enumerate(row):
+#             if val == '.':
+#                 continue
+#             else:
+#                 cols[i].append(val)
+
+#     # get boxes
+#     # this iterates over a tuple,
+#     # i is 0, then 3, then 6
+#     for i in (0, 3, 6):
+#         for j in (0, 3, 6):
+#             box = [board[x][y] for x in range(i, i+3) for y in range(j, j+3)]
+#             boxes.append(box)
+
+#     def is_valid(unit):
+#         unit = [i for i in unit if i != '.']
+#         print(unit)
+#         # if there are duplicates, set() will remove them.
+#         return len(set(unit)) == len(unit)
+
+
+#     for row in board:
+#         if is_valid(row) == False:
+#             return False
+
+#     for col in cols:
+#         if is_valid(col) == False:
+#             return False
+
+#     for box in boxes:
+#         if is_valid(box) == False:
+#             return False
+
+#     return True
+
+def isValidSudoku(board):
+    return (is_row_valid(board) and
+            is_col_valid(board) and
+            is_square_valid(board))
+
+def is_row_valid(board):
+    for row in board:
+        if not is_unit_valid(row):
+            return False
+    return True
+
+def is_col_valid(board):
+    for col in zip(*board):
+        breakpoint()
+        if not is_unit_valid(col):
+            return False
+    return True
+
+def is_square_valid(board):
+    for i in (0, 3, 6):
+        for j in (0, 3, 6):
+            square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+            if not is_unit_valid(square):
+                return False
+    return True
+
+def is_unit_valid(unit):
+    unit = [i for i in unit if i != '.']
+    # breakpoint()
+    return len(set(unit)) == len(unit)
+
+board =[["5","3","7",".",".",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
 ,[".","9","8",".",".",".",".","6","."]
 ,["8",".",".",".","6",".",".",".","3"]
@@ -44,85 +168,4 @@ matrix = [["5","3",".",".","7",".",".",".","."]
 ,[".",".",".","4","1","9",".",".","5"]
 ,[".",".",".",".","8",".",".","7","9"]]
 
-
-
-
-# def isValidSudoku(board):
-#     """
-#     :type board: List[List[str]]
-#     :rtype: bool
-#     """
-
-#     # create empty set
-#     rows = [set() for x in range(9)]
-#     columns = [set() for x in range(9)]
-#     squares = [[set() for x in range(3)] for y in range(3)]
-
-#     print(rows)
-
-#     for x in range(9):
-#         for y in range(9):
-#             cell_value = board[x][y]
-
-#             if cell_value == ".":
-#                 continue
-#             if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
-#                 return False
-
-#             rows[x].add(cell_value)
-#             columns[y].add(cell_value)
-#             squares[x//3][y//3].add(cell_value)
-
-#     print("rows ", rows)
-#     return True
-
-# print(isValidSudoku(matrix))
-
-
-def isValidSudoku(self, board):
-    """
-    :type board: List[List[str]]
-    :rtype: bool
-    """
-
-    # get column
-    # get box
-
-    cols = [[] for _ in range(9)]
-    boxes = []
-    # get cols
-    for row in board:
-        for i, val in enumerate(row):
-            if val == '.':
-                continue
-            else:
-                cols[i].append(val)
-
-    # get boxes
-    # this iterates over a tuple,
-    # i is 0, then 3, then 6
-    for i in (0, 3, 6):
-        for j in (0, 3, 6):
-            box = [board[x][y] for x in range(i, i+3) for y in range(j, j+3)]
-            boxes.append(box)
-
-    def is_valid(unit):
-        unit = [i for i in unit if i != '.']
-
-        # if there are duplicates, set() will remove them.
-        return len(set(unit)) == len(unit)
-
-
-    for row in board:
-        if is_valid(row) == False:
-            return False
-
-    for col in cols:
-        if is_valid(col) == False:
-            return False
-
-    for box in boxes:
-        if is_valid(box) == False:
-            return False
-
-    return True
+print(isValidSudoku(board))
---
+++ neetcode2/product_of_arr_except_self.py
@@ -0,0 +1,43 @@
+def productExceptSelf(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[int]
+    """
+
+    n = len(nums)
+
+    left = [1] * n
+    right = [1] * n
+
+    # 1, 2, 3
+    # RANGE ends AT n, not before
+    # start at 1
+    for i in range(1,n):
+        left[i] = left[i-1] * nums[i - 1]
+        # left[1] = left[0] * nums[0]
+        #         = [1]     * 1
+        # left[2] = left[1] * nums[1]
+        #         = 1       *    2
+        # left[3] = left[2] * nums[2]
+        #         = 2    *       3
+
+    #  2 , 1, 0
+    # start at 2nd from end
+    for i in range(n-2, -1, -1):
+        print(i)
+        right[i] = right[i+1] * nums[i+1]
+        # right[2] = right[3] * nums[3]
+        # right[2] =     1    *   4
+        # right[1] = right[2] * nums[2]
+        # right[1] =   4      *    3
+        # right[0] = right[1] * nums[1]
+        # right[0] =    12    *  2
+
+    # left = [1, 1, 2, 6]
+    # right= [24, 12, 4, 1]
+    result = [left[i] * right[i] for i in range(n)]
+
+    return result
+
+nums = [1,2,3,4]
+print(productExceptSelf(nums))
